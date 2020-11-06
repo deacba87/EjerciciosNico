52b715714b338c2d59ec72d986a9de81122151e6
deacba87 <deacba1987@gmail.com>
2020-11-06 10:22:14 -0300

    ejercicio 3 - v1

---
+++ Ejercicio 3/Login/app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.example.login
+/* Notas
+1- En algun lado lei que kotlin me debería tomar los elementos del container directamente sin necesidad de usar el findView, pero mis muñones no logran hacerlo andar
+2- Se que el Toast solo es mas feo que golpearse el dedo pequeño, pero me diste in dia para hacer esto y en la hora de trabajo estoy haciendo los cursos
+3- Pense que la propiedad onClic de boton me iba a permitir no hacer el listener en el onCreate, pero me da error de instalacion.. Todavia no arranque el curso de kotlin en androoid (estoy por el BASICO), asi que no le prestes atencion a esta burrada mia
+ */
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+
+class MainActivity : AppCompatActivity()
+{
+    private val RC_SIGN_IN = 1
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnLogin: Button = findViewById(R.id.btnIngresar)
+        btnLogin.setOnClickListener(
+                View.OnClickListener { validarLogin() }
+        )
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+    }
+
+    override fun onStart()
+    {
+        super.onStart()
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        var text: String
+        if (account == null)
+        {
+            text = "No logueado"
+        }
+        else
+        {
+            text = "Logueado"
+        }
+        Toast.makeText(this.applicationContext, text, Toast.LENGTH_LONG).show()
+    }
+
+    fun login(view: View)
+    {
+        validarLogin()
+    }
+
+    private fun validarLogin()
+    {
+        var oValidacion: ValidacionLogin = ValidacionLogin()
+        var usuario: TextView = findViewById(R.id.txtUsuario)
+        var pass: TextView = findViewById(R.id.txtPass)
+        val toast: Toast
+
+        val (a, b) = oValidacion.validarIntegridad(usuario.text.toString(), pass.text.toString())
+
+        if (a == true)
+        {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+        else
+        {
+            Toast.makeText(this.applicationContext, b.toString(), Toast.LENGTH_LONG)
+        }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+    {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN)
+        {
+            Toast.makeText(this.applicationContext, "Login correcto", Toast.LENGTH_LONG)
+        }
+    }
+
+}
---
+++ Ejercicio 3/Login/app/src/main/java/com/example/login/ValidacionLogin.kt
@@ -0,0 +1,227 @@
+package com.example.login
+
+import android.content.Intent
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+
+/*Notas:
+1- Si, se que pude haber definido los caracteres especiales usando "\'" "Ä" etc... pero me odio mucho aparentemente, y en un lugar lei de usar el codigo html
+2- Se que no es necesario los {} cuando el if es de una linea, pero... caprichos
+3- La funcion correcta es la que se va a encargar de validar si el pass y usuario son incorrectos
+*/
+class ValidacionLogin
+{
+    private var lstCaracteres: ArrayList<String> = initCaracteres()
+
+    private fun initCaracteres(): ArrayList<String>
+    {
+        //Comillas simples ni dobles, coma, punto y coma, diéresis, tildes, caracteres de escapes
+
+        val comillaSimple: String = "\u0027"
+        val comillaDoble: String = "\u0022"
+        val coma: String = "\u002C"
+        val puntoComa: String = "\u003B"
+        val dieresisMayusA: String = "\u00C4"
+        val dieresisMinusA: String = "\u00E4"
+        val dieresisMayusE: String = "\u00CB"
+        val dieresisMinusE: String = "\u00EB"
+        val dieresisMayusI: String = "\u00CF"
+        val dieresisMinusI: String = "\u00EF"
+        val dieresisMayusO: String = "\u00D6"
+        val dieresisMinusO: String = "\u00F6"
+        val dieresisMayusU: String = "\u00D6"
+        val dieresisMinusU: String = "\u00F6"
+        val tildeMayusA: String = "\u00C1"
+        val tildeMinusA: String = "\u00E1"
+        val tildeMayusE: String = "\u00C9"
+        val tildeinusE: String = "\u00E9"
+        val tildeMayusI: String = "\u00CD"
+        val tildeMinusI: String = "\u00ED"
+        val tildeMayusO: String = "\u00D3"
+        val tildeMinusO: String = "\u00F3"
+        val tildeMayusU: String = "\u00DA"
+        val tildeMinusU: String = "\u00FA"
+        val tab: String = "\t"
+        val backspace: String = "\b"
+        val newline: String = "\n"
+        val carriage: String = "\r"
+
+        var lstCaract: ArrayList<String> = ArrayList()
+
+        lstCaract.add(comillaSimple)
+        lstCaract.add(comillaDoble)
+        lstCaract.add(coma)
+        lstCaract.add(puntoComa)
+        lstCaract.add(dieresisMayusA)
+        lstCaract.add(dieresisMinusA)
+        lstCaract.add(dieresisMayusE)
+        lstCaract.add(dieresisMinusE)
+        lstCaract.add(dieresisMayusI)
+        lstCaract.add(dieresisMinusI)
+        lstCaract.add(dieresisMayusO)
+        lstCaract.add(dieresisMinusO)
+        lstCaract.add(dieresisMayusU)
+        lstCaract.add(dieresisMinusU)
+        lstCaract.add(tildeMayusA)
+        lstCaract.add(tildeMinusA)
+        lstCaract.add(tildeMayusE)
+        lstCaract.add(tildeinusE)
+        lstCaract.add(tildeMayusI)
+        lstCaract.add(tildeMinusI)
+        lstCaract.add(tildeMayusO)
+        lstCaract.add(tildeMinusO)
+        lstCaract.add(tildeMayusU)
+        lstCaract.add(tildeMinusU)
+        lstCaract.add(tab)
+        lstCaract.add(backspace)
+        lstCaract.add(newline)
+        lstCaract.add(carriage)
+
+        return lstCaract
+    }
+
+    public fun validarIntegridad(iUser: String, iPass: String):Pair<Boolean, String>
+    {
+        var msgError: String = "";
+        var user: String = "";
+        var pass: String = "";
+
+        // Usuario y Password no deben estar vacios
+        if (isNull(iUser))
+        {
+            return Pair(false, "Usuario no puede ser vacio")
+        }
+
+        if (isNull(iPass))
+        {
+            return Pair(false, "Contraseña no puede ser vacio")
+        }
+
+        //Usuario no puede tener los siguientes caracteres : Comillas simples ni dobles, coma, punto y coma, diéresis, tildes, caracteres de escapes.
+        if (haveSpecialChar(iUser))
+        {
+            return Pair(false, "Usuario no puede contener caracteres especiales")
+        }
+
+        //El usuario debe validar un mínimo de 8 caracteres
+        if (iUser.length < 8)
+        {
+            return Pair(false, "Usuario debe tener más de 8(ocho) caracteres")
+        }
+
+        //El password tiene las mismas restricciones pensionadas para usuario
+        if (iPass.length < 8)
+        {
+            return Pair(false, "Contraseña debe tener más de 8(ocho) caracteres")
+        }
+
+        if (haveSpecialChar(iUser))
+        {
+            return Pair(false, "Usuario no puede contener caracteres especiales")
+        }
+
+        //El password debe validar un mínimo de 8 caracteres, 1 mayúscula, 1 minúscula, 1 numero y algún carácter especial que no este contemplado en la lista de restricciones.
+        if (!securityPass(iPass))
+        {
+            return Pair(
+                false,
+                "La contraseña debe contener mayuscula, minuscula, numero, caracter especial, sangre de una virgen, una cancion de los 80 o 90"
+            )
+        }
+
+
+        if (correcta(iUser, iPass))
+        {
+            return Pair(true, "Login exitoso")
+        }
+        else
+        {
+            return Pair(false, "Usuario y pass incorrectos")
+        }
+
+    }
+
+    private fun correcta(iUser: String, iPass: String): Boolean
+    {
+        return true
+    }
+
+    private fun isNull(iTexto: String):Boolean
+    {
+        var texto: String
+        texto = iTexto.replace("\\s".toRegex(), "")
+        if (texto.isEmpty()) {
+            return true
+        }
+        return false
+    }
+
+    private fun haveSpecialChar(iTexto: String): Boolean
+    {
+        var ch:String = ""
+        for (c in iTexto)
+        {
+            ch = c.toString()
+
+            for (item in  lstCaracteres)
+            {
+                if (item.equals(ch))
+                {
+                    return true
+                }
+            }
+
+        }
+        return false
+    }
+
+    private fun securityPass(iTexto: String):Boolean
+    {
+        //El password debe validar un mínimo de 8 caracteres, 1 mayúscula, 1 minúscula, 1 numero y algún carácter especial que no este contemplado en la lista de restricciones.
+        var haveMayus: Boolean = false
+        var haveMinus: Boolean = false
+        var haveNumber: Boolean = false
+        var haveSpecial: Boolean = false
+
+        var ch: String = ""
+        for (c in iTexto)
+        {
+            if ( c.isLetter() &&
+                ( haveMayus == false || haveMinus == false ) )
+            {
+                if (c.isUpperCase())
+                {
+                    haveMayus = true
+                }
+                else
+                {
+                    haveMinus = true
+                }
+            }
+            else if ( c.isDigit() && haveNumber == false)
+            {
+                haveNumber = true
+            }
+            else if (haveSpecial == false)
+            {
+                if (!lstCaracteres.contains(c.toString()))
+                {
+                    haveSpecial = true
+                }
+            }
+
+        }
+
+        if (haveMayus && haveMayus && haveNumber && haveSpecial )
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+
+
+    }
+}
+
